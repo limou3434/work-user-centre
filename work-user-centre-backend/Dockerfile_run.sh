ab9ff645982f90461b6536f89a297a357f2e7a86
limou3434 <898738804@qq.com>
2025-03-21 09:23:25 +0800

    重新 Docker脚本

--- work-user-centre-backend/Dockerfile_run.sh
+++ work-user-centre-backend/Dockerfile_run.sh
@@ -2,6 +2,6 @@
 # ./Dockerfile_run.sh
 docker container stop work-user-centre-backend || true
 docker container rm work-user-centre-backend || true
-docker run -d --network host --name work-user-centre-backend work-user-centre-backend:0.0.1
+docker run -d --restart=always --network host --name work-user-centre-backend work-user-centre-backend:0.0.1
 docker container logs work-user-centre-backend
 docker container ls -a
--- work-user-centre-frontend/Dockerfile_run.sh
+++ work-user-centre-frontend/Dockerfile_run.sh
@@ -2,6 +2,6 @@
 # ./Dockerfile_run.sh
 docker container stop work-user-centre-frontend || true
 docker container rm work-user-centre-frontend || true
-docker run -d --network host --name work-user-centre-frontend work-user-centre-frontend:0.0.1
+docker run -d --restart=always --network host --name work-user-centre-frontend work-user-centre-frontend:0.0.1
 docker container logs work-user-centre-frontend
 docker container ls -a
