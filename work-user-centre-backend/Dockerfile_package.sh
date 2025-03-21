09d5681d0c130d6ef4f7bcf8cc0a242d7ea3d52f
limou3434 <898738804@qq.com>
2025-03-21 12:06:39 +0800

    修改打包脚本

--- work-user-centre-backend/Dockerfile_package.sh
+++ work-user-centre-backend/Dockerfile_package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ./Dockerfile_package.sh
-docker save -o work-user-centre-backend.tar work-user-centre-backend:0.0.1
+docker save -o work-user-centre-backend-docker-image-0.0.1.tar work-user-centre-backend:0.0.1
