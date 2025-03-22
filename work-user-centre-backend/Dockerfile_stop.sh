9909d938ca96ef54313fd93de2c32ed4572741f7
limou3434 <898738804@qq.com>
2025-03-22 08:18:55 +0800

    修复脚本错误

--- work-user-centre-backend/Dockerfile_stop.sh
+++ work-user-centre-backend/Dockerfile_stop.sh
@@ -2,6 +2,6 @@
 # 镜像终止脚本
 sudo docker container stop work-user-centre-backend || true
 sudo docker container rm work-user-centre-backend || true
-sudo docker container logs work-user-centre-frontend
+sudo docker container logs work-user-centre-backend
 sudo docker container ls -a
 echo "脚本结束"
