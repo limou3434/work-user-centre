c6fc4449a24faf73148781d1252be4103f50bc5f
xiaogithubooo <898738804@qq.com>
2025-03-30 22:11:07 +0800

    一些小的改动

--- backend/src/main/java/com/work/workusercentre/WorkUserCentreApplication.java
+++ backend/src/main/java/com/work/workusercentre/WorkUserCentreApplication.java
@@ -14,8 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.work.workusercentre.mapper") // 启用 MyBatisPlus 扫描 ./src/Mapper/ 中的映射
 @Slf4j
 public class WorkUserCentreApplication {
+
     public static void main(String[] args) {
         /* var context = */ SpringApplication.run(WorkUserCentreApplication.class, args);
-        log.info("访问 http://localhost:8000/work_tongue_diagnosis_api/doc.html 即可得到在线文档");
+        log.info("访问 http://localhost:8000/work_user_centre_api/doc.html 即可得到在线文档");
     }
+
 }
